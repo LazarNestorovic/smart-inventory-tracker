4ec2ee70c6631025a7536088bc96b2791845add1
Lazar Nestorovic <lazarnestorovvic003@gmail.com>
2025-08-12 17:28:52 +0200

    DTOs added

---
+++ DTOs/CategoryDTO.cs
@@ -0,0 +1,66 @@
+﻿using SmartInventoryTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartInventoryTracker.DTOs
+{
+    public class CategoryDTO
+    {
+        private int id;
+        public int Id
+        {
+            get => id;
+            set
+            {
+                if (value != id)
+                {
+                    id = value;
+                }
+            }
+        }
+        private string name;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (value != name)
+                {
+                    name = value;
+                }
+            }
+        }
+        private ObservableCollection<ProductDTO> products;
+        public ObservableCollection<ProductDTO> Products
+        {
+            get => products;
+            set
+            {
+                if (value != products)
+                {
+                    products = value;
+                }
+            }
+        }
+        public CategoryDTO(Category category)
+        { 
+            Id = category.Id;
+            Name = category.Name;
+            Products = new ObservableCollection<ProductDTO>(
+                category.Products.Select(p => new ProductDTO(p)));
+        }
+        public Category ToCategory() 
+        { 
+            return new Category
+            {
+                Id = this.Id,
+                Name = this.Name,
+                Products = this.Products.Select(dto => dto.ToProduct()).ToList()
+            };
+        }
+    }
+}
---
+++ Views/LoginWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SmartInventoryTracker.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordBox.Password;
+
+            // Ova logika je primer - u pravoj aplikaciji koristiš sigurnu proveru
+            if (username == "admin" && password == "password")
+            {
+                // Navigacija na MainPage
+                //NavigationService.Navigate(new MainPage());
+                MessageBox.Show("User exits");
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password", "Login Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
